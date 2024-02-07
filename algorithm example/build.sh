dd57955725d0ca522dd9f4c95d27e2f372a13021
kev98 <kevinmarchesini@gmail.com>
2024-02-07 20:47:49 +0100

    Minimal algorithm example

--- algorithm example/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-# docker build --no-cache -t toothfairy_algorithm "$SCRIPTPATH"
-docker build -t toothfairy_algorithm "$SCRIPTPATH"
